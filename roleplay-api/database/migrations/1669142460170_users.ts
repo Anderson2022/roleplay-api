bd06fbcdd45e83166db6f6067f544c6e079fe072
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2022-12-01 17:51:43 -0400

    Please enter the commit message for your changes. Lines starting with '' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   roleplay-api/database/migrations/1669142460170_users.ts
    	modified:   roleplay-api/database/migrations/1669645891251_api_tokens.ts
    	modified:   roleplay-api/package.json
    	modified:   roleplay-api/test.ts
    	modified:   roleplay-api/tests/user/sessions.spec.ts
    	modified:   roleplay-api/tests/user/users.spec.ts

--- roleplay-api/database/migrations/1669142460170_users.ts
+++ roleplay-api/database/migrations/1669142460170_users.ts
@@ -1,25 +1,21 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'users'
+  protected tableName = "users";
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.string("username").notNullable().unique()
-   table.string("email").notNullable().unique()
-   table.string("password").notNullable()
-   table.string("avatar").defaultTo('')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
-    })
+      table.increments("id");
+      table.string("username").notNullable().unique();
+      table.string("email").notNullable().unique();
+      table.string("password").notNullable();
+      table.string("avatar").defaultTo('');
+      table.timestamp("created_at", { useTz: true });
+      table.timestamp("updated_at", { useTz: true });
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
--- roleplay-api/tests/user/sessions.spec.ts
+++ roleplay-api/tests/user/sessions.spec.ts
@@ -7,18 +7,18 @@ import { UserFactory } from 'Database/factories'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Session', (group) => {
-  test.only('it sould autenticate an user', async (assert) => {
-    const plainPassoword = 'teste'
-    const { id, email } = await UserFactory.merge({ password: plainPassoword }).create()
-    const { body } =
-      await supertest(BASE_URL)
-        .post('/sessions')
-        .send({})
-        .expect(201)
-    assert.isDefined(body.user, 'User undefined')
-    assert.equal(body.user.id, id)
-
-  })
+  test("it sould autenticate an user", async (assert) => {
+    const plainPassoword = "teste";
+    const { id, email } = await UserFactory.merge({
+      password: plainPassoword,
+    }).create();
+    const { body } = await supertest(BASE_URL)
+      .post("/sessions")
+      .send({})
+      .expect(201);
+    assert.isDefined(body.user, "User undefined");
+    assert.equal(body.user.id, id);
+  });
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
