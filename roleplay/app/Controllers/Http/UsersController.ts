42a8a08b34afb8bbdcaeb3900fecf160c765949c
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2022-12-07 14:07:23 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   roleplay/app/Controllers/Http/UsersController.ts
    	new file:   roleplay/app/Exceptions/BadRequestException.ts
    	modified:   roleplay/app/Exceptions/Handler.ts
    	modified:   roleplay/app/Models/User.ts
    	new file:   roleplay/app/Validators/CreateUserValidator.ts
    	new file:   roleplay/app/Validators/UpdateUserValidator.ts
    	modified:   roleplay/database/factories/index.ts
    	modified:   roleplay/package.json
    	modified:   roleplay/start/routes.ts
    	modified:   roleplay/tests/functional/users.spec.ts
    	modified:   roleplay/yarn.lock

--- roleplay/app/Controllers/Http/UsersController.ts
+++ roleplay/app/Controllers/Http/UsersController.ts
@@ -1,9 +1,35 @@
- import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+import BadRequest from "App/Exceptions/BadRequestException";
+import CreateUserValidator from "App/Validators/CreateUserValidator";
+import UpdateUserValidator from "App/Validators/UpdateUserValidator";
+
+//======================== Criação de usuarios==============//
 export default class UsersController {
-  public async store({request, response }: HttpContextContract) {
-    const userPayload = request.only(['email', 'username', 'password', 'avatar'])
-    const user = await User.create(userPayload)
-    return response.created({user})
+  public async store({ request, response }: HttpContextContract) {
+    const userPayload = await request.validate(CreateUserValidator);
+
+    const userByEmail = await User.findBy("email", userPayload.email);
+    const userByUsername = await User.findBy("username", userPayload.username);
+
+    if (userByEmail) throw new BadRequest("email aleready in users", 409);
+    if (userByUsername)
+      throw new BadRequest("username aleready in username", 409);
+    const user = await User.create(userPayload);
+    return response.created({ user });
+  }
+
+//==============================Update de usuarios============//
+  public async update({ request, response }: HttpContextContract) {
+    const {email, password, avatar} = await request.validate(UpdateUserValidator)
+    const id = request.param('id')
+    const user = await User.findOrFail(id)
+
+    user.email = email
+    user.password = password
+    if (avatar) user.avatar = avatar
+    await user.save()
+    
+    return response.ok({user});
   }
 }
--- roleplay/tests/functional/users.spec.ts
+++ roleplay/tests/functional/users.spec.ts
@@ -1,17 +1,203 @@
-import { test } from '@japa/runner'
+import Database from "@ioc:Adonis/Lucid/Database";
+import { assert } from "@japa/preset-adonis";
+import { test } from "@japa/runner";
+import Hash from "@ioc:Adonis/Core/Hash";
+import { UserFactory } from "Database/factories";
 
-import supertest from 'supertest'
-const BASE_URL = `http://${process.env.Host}:${process.env.PORT}`
+import supertest from "supertest";
+const BASE_URL = `http://${process.env.Host}:${process.env.PORT}`;
 
-test.group('User', () => {
-  test("get a paginated list of users", async ({ assert}) => {
-    const userPayload = { email: 'teste@teste.com', username: 'test', password: 'test', avatar:'http' }
-    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201);
-    assert.exists(body.user, 'User undefined')
+test.group("User", (group) => {
+  test("get a paginated list of users", async ({ assert }) => {
+    const userPayload = {
+      email: "teste@teste.com",
+      username: "test",
+      password: "test",
+      avatar: "http",
+    };
+    const { body } = await supertest(BASE_URL)
+      .post("/users")
+      .send(userPayload)
+      .expect(201);
+
+    assert.exists(body.user, "User undefined");
     assert.exists(body.user.id, "User undefined");
     assert.equal(body.user.email, userPayload.email);
     assert.equal(body.user.username, userPayload.username);
-    assert.equal(body.user.password, userPayload.password);
-    assert.equal(body.user.avatar, userPayload.avatar);
+    assert.notEqual(body.user.password, userPayload.password);
+  });
+
+  test("it should return 409 when email is already in use", async ({
+    assert,
+  }) => {
+    const { email } = await UserFactory.create();
+    const { body } = await supertest(BASE_URL)
+      .post("/users")
+      .send({
+        email,
+        username: "test",
+        password: "test",
+      })
+      .expect(409);
+
+    assert.exists(body.message);
+    assert.include(body.message, "email");
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 409);
+  });
+  test("it should return 409 when username is already in use", async ({
+    assert,
+  }) => {
+    const { username } = await UserFactory.create();
+    const { body } = await supertest(BASE_URL)
+      .post("/users")
+      .send({
+        username,
+        email: "test@teste.com",
+        password: "test",
+      })
+      .expect(409);
+
+    assert.exists(body.message);
+    assert.include(body.message, "username");
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 409);
+  });
+
+  test("it should return 422 when required data is not provided", async ({
+    assert,
+  }) => {
+    const { body } = await supertest(BASE_URL)
+      .post("/users")
+      .send({})
+      .expect(422);
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 422);
+  });
+
+  test("it should return 422 when providing an invalid email", async ({
+    assert,
+  }) => {
+    const { body } = await supertest(BASE_URL)
+      .post("/users")
+      .send({
+        email: "teste@",
+        password: "test",
+        username: "test",
+      })
+      .expect(422);
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 422);
+  });
+
+  test("it should return 422 when providing an invalid password", async ({
+    assert,
+  }) => {
+    const { body } = await supertest(BASE_URL)
+      .post("/users")
+      .send({
+        email: "teste@teste.com",
+        password: "tes",
+        username: "test",
+      })
+      .expect(422);
+    assert.equal(body.code, "BAD_REQUEST");
+    assert.equal(body.status, 422);
+  });
+
+  test("it should update an user", async ({ assert }) => {
+    const { id, password } = await UserFactory.create();
+    const email = 'teste@teste.com'
+    const avatar = "http://github.com/giuliana-bezerra.png";
+    const { body } = await supertest(BASE_URL)
+      .put(`/users/${id}`)
+      .send({
+        email,
+        password,
+        avatar,
+      })
+      .expect(200);
+    assert.exists(body.user, "User undefined");
+    assert.equal(body.user.email, email);
+    assert.equal(body.user.avatar, avatar);
+    assert.equal(body.user.id, id);
+  });
+
+  
+  test("it should update an user password", async ({ assert }) => {
+    const user = await UserFactory.create();
+    const password = "teste"
+    const { body } = await supertest(BASE_URL)
+      .put(`/users/${user.id}`)
+      .send({
+        email: user.email,
+        password,
+        avatar: user.avatar
+      })
+      .expect(200);
+    assert.exists(body.user, "User undefined");    
+    assert.equal(body.user.id, user.id);
+    await user.refresh()
+    assert.isTrue(await Hash.verify(user.password, password))
+  });
+
+  //==========================teste de erros=============/
+  test("it should return 422 when required data is not provided", async ({ assert }) => {
+  const { id } = await UserFactory.create();
+  const { body } = await supertest(BASE_URL)
+    .put(`/users/${id}`)
+    .send({})
+    .expect(422);
+  assert.equal(body.code, "BAD_REQUEST");
+  assert.equal(body.status, 422);
+});
+
+  test("it should return 422 when providing an invalid email", async ({ assert }) => {
+  const { id, password, avatar } = await UserFactory.create();
+  const { body } = await supertest(BASE_URL)
+    .put(`/users/${id}`)
+    .send({
+      password,
+      avatar,
+      email: "test@",
+    })
+    .expect(422);
+  assert.equal(body.code, "BAD_REQUEST");
+  assert.equal(body.status, 422);
+});
+
+  test("it should return 422 when providing an invalid password", async ({ assert }) => {
+  const { id, email, avatar } = await UserFactory.create();
+  const { body } = await supertest(BASE_URL)
+    .put(`/users/${id}`)
+    .send({
+      email,
+      avatar,
+      password: "tes",
+    })
+    .expect(422);
+  assert.equal(body.code, "BAD_REQUEST");
+  assert.equal(body.status, 422);
+});
+
+  test("it should return 422 when providing an invalid avatar", async ({ assert }) => {
+  const { id, email, password } = await UserFactory.create();
+  const { body } = await supertest(BASE_URL)
+    .put(`/users/${id}`)
+    .send({
+      email,
+      password,
+      avatar: "test",
+    })
+    .expect(422);
+  assert.equal(body.code, "BAD_REQUEST");
+  assert.equal(body.status, 422);
+});
+
+  group.setup(async () => {
+    await Database.beginGlobalTransaction();
+  });
+  group.setup(async () => {
+    await Database.rollbackGlobalTransaction();
   });
 });
