68d34469ee9a1333b48486d318672466a09610ee
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2022-12-13 13:16:33 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .vscode/launch.json
    	modified:   roleplay/.adonisrc.json
    	new file:   roleplay/app/Controllers/Http/SessionsController.ts
    	modified:   roleplay/app/Exceptions/Handler.ts
    	new file:   roleplay/app/Middleware/Auth.ts
    	new file:   roleplay/app/Middleware/SilentAuth.ts
    	new file:   roleplay/config/auth.ts
    	new file:   roleplay/contracts/auth.ts
    	new file:   roleplay/database/migrations/1670940125028_api_tokens.ts
    	modified:   roleplay/package-lock.json
    	modified:   roleplay/package.json
    	modified:   roleplay/start/routes.ts
    	new file:   roleplay/tests/functional/users/sessions.spec.ts
    	modified:   roleplay/tsconfig.json
    	modified:   roleplay/yarn.lock

--- roleplay/app/Exceptions/Handler.ts
+++ roleplay/app/Exceptions/Handler.ts
@@ -23,21 +23,35 @@ export default class ExceptionHandler extends HttpExceptionHandler {
     super(Logger);
   }
 
-  public async handle(error: Exception, ctx: HttpContextContract): Promise<any> {
+  public async handle(
+    error: Exception,
+    ctx: HttpContextContract
+  ): Promise<any> {
+  
     if (error.status === 422)
       return ctx.response.status(error.status).send({
         code: "BAD_REQUEST",
         message: error.message,
         status: error.status,
-        errors: error['messages']?.errors ? error['messages'].errors : '',
+        errors: error["messages"]?.errors ? error["messages"].errors : "",
       });
-    
-    else if (error.code === 'E_ROW_NOT_FOUND')
+    else if (error.code === "E_ROW_NOT_FOUND")
       return ctx.response.status(error.status).send({
-        code: 'BAD_REQUEST',
-        message: 'resource not found',
+        code: "BAD_REQUEST",
+        message: "resource not found",
         status: 404,
-      })
+      });
+    else if (
+      ["E_INVALID_AUTH_UID", "E_INVALID_AUTH_PASSWORD"].includes(
+        error.code || ""
+      )
+    )
+      return ctx.response.status(error.status).send({
+        code: "BAD_REQUEST",
+        message: "invalid credentials",
+        status: 400,
+      });
+
     return super.handle(error, ctx);
   }
 }
